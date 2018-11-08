278adfabdae4e76deff884442a9b64faabebe911
Erick Hideki Nishimoto <ti-erick@ftd.com.br>
2018-11-08 13:30:10 -0200

    Add erro de mysql

--- conecta.php
+++ conecta.php
@@ -1,5 +1,6 @@
 <?php
 
-$conexao = mysqli_connect('mysql942.umbler.com:41890', 'erick.nishimoto', 'Ehn*170890', 'erick_bd');
+@$conexao = mysqli_connect('mysql942.umbler.com:41890', 'erick.nishimoto', 'Ehn*170890', 'erick_bd') or die(
+    header("Location: erro?mysql=".mysqli_connect_errno()));
 mysqli_set_charset($conexao,"utf8");
 mysqli_query($conexao, "SET lc_time_names = 'pt_BR';"); 
